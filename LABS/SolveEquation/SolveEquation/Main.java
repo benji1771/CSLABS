cdcbe068510b3b6bca60678bebde2b2b0f3610e4
benji <sarolbenjamin_08221999@yahoo.com>
2021-10-12 23:41:02 -0500

    Working SolveEquation

--- LABS/SolveEquation/SolveEquation/Main.java
+++ LABS/SolveEquation/SolveEquation/Main.java
@@ -4,34 +4,43 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> first, second, temp;
-        temp = new ArrayList<>();
+        ArrayList<String> first, second;
+        Long temp = 0L;
         first = new ArrayList<>(Arrays.asList(scanner.nextLine().split(" ")));
         second = new ArrayList<>(Arrays.asList(scanner.nextLine().split(" ")));
+        
         allPermutations(first, temp, second);
         System.out.println(answer);
     }
     static String answer = "no";
-
-    public static void allPermutations(ArrayList<String> original, ArrayList<String> permList, ArrayList<String> myList) {     
-        if(permList.size() >= original.size()){
-           long sum = 0L; 
-           for(int k = 0; k < permList.size(); k++){
-                sum+=(Long.parseLong(original.get(k)) * Long.parseLong(permList.get(k)));
+    static int index = 0;
+    public static void allPermutations(ArrayList<String> original, Long sum, ArrayList<String> myList) {     
+        String temp;
+        
+        if(index >= original.size() - 1){ 
+            //System.out.println(myList);
+            
+           for(int i = 0; i < myList.size(); i++){
+               if(sum + (Long.parseLong(myList.get(i)) * Long.parseLong(original.get(original.size() - 1))) == 0L){
+                  answer = "yes";
+                  return;
+               }
            }
-           if(sum == 0L){
-              answer = "yes";
-              return;
-           }
+           
         }else{       
            for(int i = 0; i < myList.size(); i++){
-              permList.add(myList.remove(i));
-              allPermutations(original, permList, myList);
-              myList.add(i, permList.remove(permList.size() - 1));
+              temp = myList.remove(i);
+              sum+=(Long.parseLong(original.get(index)) * Long.parseLong(temp));
+              index++;
+              allPermutations(original, sum, myList);
+              index--;
+              myList.add(i, temp);
+              sum-=(Long.parseLong(original.get(index)) * Long.parseLong(temp));
            }
            
         }
