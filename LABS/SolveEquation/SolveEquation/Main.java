e2575b37e7cd32e4bc673a513f38584b6a044f82
benji <sarolbenjamin_08221999@yahoo.com>
2021-10-12 21:24:37 -0500

    minor fix

--- LABS/SolveEquation/SolveEquation/Main.java
+++ LABS/SolveEquation/SolveEquation/Main.java
@@ -5,21 +5,20 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> first, second, temp;
         temp = new ArrayList<>();
         first = new ArrayList<>(Arrays.asList(scanner.nextLine().split(" ")));
         second = new ArrayList<>(Arrays.asList(scanner.nextLine().split(" ")));
-        
         allPermutations(first, temp, second);
-
         System.out.println(answer);
     }
     static String answer = "no";
 
     public static void allPermutations(ArrayList<String> original, ArrayList<String> permList, ArrayList<String> myList) {     
-        if(permList.size() == original.size()){
+        if(permList.size() >= original.size()){
            long sum = 0L; 
            for(int k = 0; k < permList.size(); k++){
                 sum+=(Long.parseLong(original.get(k)) * Long.parseLong(permList.get(k)));
