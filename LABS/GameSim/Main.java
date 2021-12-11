67009d68649df430b01822c3a4ac7725ec4ef037
benji <sarolbenjamin_08221999@yahoo.com>
2021-12-10 19:37:22 -0600

    gamesim

--- LABS/GameSim/Main.java
+++ LABS/GameSim/Main.java
@@ -12,9 +12,10 @@ public class Main {
         for(int i = 0; i < testCases; i++){
             PriorityQueue<Integer> redTeam = new PriorityQueue<Integer>(Collections.reverseOrder());
             PriorityQueue<Integer> blueTeam = new PriorityQueue<Integer>(Collections.reverseOrder());
-            int location = scnr.nextInt();
+            int[] locations = new int[scnr.nextInt()];
             int sizeRed = scnr.nextInt();
             int sizeBlue = scnr.nextInt();
+
             for(int mem = 0; mem < sizeRed; mem++){
                 redTeam.add(scnr.nextInt());
             }
@@ -22,20 +23,43 @@ public class Main {
                 blueTeam.add(scnr.nextInt());
             }
             while(redTeam.size() > 0 && blueTeam.size() > 0){
-                for(int l = 0; l < location; l++){
-                    if(redTeam.size() == 0){
-
-                    }else if(blueTeam.size() == 0){
-
+                for(int l = 0; l < locations.length; l++){
+                    if(redTeam.isEmpty() && blueTeam.isEmpty()){
+                        locations[l] = 0;
+                    }else if(redTeam.isEmpty()){
+                        locations[l] = blueTeam.poll() * -1;
+                    }else if(blueTeam.isEmpty()){
+                        locations[l] = redTeam.poll();
                     }else{
-                        int sum = redTeam.poll() - blueTeam.poll();
-                        if(sum > 0){
-                            
-                        }
+                        locations[l] = redTeam.poll() - blueTeam.poll();
+                    }
+                }
+                for(int s = 0; s < locations.length; s++){
+                    if(locations[s] > 0){
+                        redTeam.add(locations[s]);
+                    }else if(locations[s] < 0){
+                        blueTeam.add(locations[s] * -1);
                     }
                 }
             }
+            if(redTeam.isEmpty() && blueTeam.isEmpty()){
+                System.out.println("draw");
+            }else if(redTeam.isEmpty()){
+                System.out.println("blue wins");
+                printTeam(blueTeam);
+            }else{
+                System.out.println("red wins");
+                printTeam(redTeam);
+            }
+            if(i != testCases - 1)
+            System.out.println();
         }
 
+    }
+
+    public static void printTeam(PriorityQueue<Integer> team){
+        while(!team.isEmpty()){
+            System.out.println(team.poll());
+        }
     }
 }
